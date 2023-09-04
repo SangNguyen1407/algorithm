dcd2d601ff104900af2a905ce751ccd3b5cf9941
SangNguyen1407 <14.sangnguyen.07@gmail.com>
2023-09-04 23:50:09 +0900

    bubble sort

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string.h>
+
+
+using namespace std;
+
+int bubbleSort(int *arr, int arr_size)
+{
+	if (arr == NULL && arr_size < 1){
+		return 0;
+	}
+	
+	int temp = 0;
+	for(int i = 0; i < arr_size; i++){
+		for (int j = arr_size-1; j > i; j--){
+			if(arr[j] < arr[j-1]){
+				temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	return 1;
+}
+
+int main (){
+	int arr[5] = {9,2,3,4};
+	int ret = bubbleSort(arr, 4);
+
+	cout << "return " << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3] << " " << endl;
+}
