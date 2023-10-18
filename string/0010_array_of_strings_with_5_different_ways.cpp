8ab63ba80719fe6217c9aab9dee4b239a9fc3c65
SangNguyen1407 <50044154+SangNguyen1407@users.noreply.github.com>
2023-10-19 00:19:07 +0900

    update file 0010

--- string/0010_array_of_strings_with_5_different_ways.cpp
+++ string/0010_array_of_strings_with_5_different_ways.cpp
@@ -12,22 +12,26 @@ int main()
     std::vector<string> colour_4{ "Blue", "Orange", "Red", "Yellow" };
     int i = 0;
     cout << endl << "colour with *[]" << endl;
-    for ( i = 0; i<4; i++){
+    for ( i = 0; i<4; i++ ){
         cout << colour_1[i] << endl;
     }
-
+    
     cout << endl << "colour with [][]" << endl;
-    for ( i = 0; i<4; i++){
+    for ( i = 0; i<4; i++ ){
         cout << colour_2[i] << endl;
     }
 
     cout << endl << "colour with string" << endl;
-    for ( i = 0; i<4; i++){
+    for ( i = 0; i<4; i++ ){
         cout << colour_3[i] << endl;
     }
 
     cout << endl << "colour with vector" << endl;
-    for ( i = 0; i<4; i++){
+    for ( i = 0; i<4; i++ ){
+        cout << colour_4[i] << endl;
+    }
+    colour_4.push_back("Pink");
+    for ( i = 0; i<5; i++ ){
         cout << colour_4[i] << endl;
     }
 }
