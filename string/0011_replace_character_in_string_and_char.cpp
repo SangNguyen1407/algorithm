4d9f895758deddffd373a2370cb92d383e1dc71e
SangNguyen1407 <50044154+SangNguyen1407@users.noreply.github.com>
2023-10-19 23:39:23 +0900

    add file 0011

---
+++ string/0011_replace_character_in_string_and_char.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string str_test = "TestForAll";
+    char* char_test = "TestForAll";
+
+    // 1. replace a character in string and char*
+    str_test[1] = 'E';
+    cout << "str_test: " << str_test << endl; 
+    for (int i=0; i < str_test.size(); i++){
+        cout << str_test[i] << " ";
+    }
+    cout << endl;
+
+    cout << "char_test: " << char_test << endl; 
+    // str_test[1] = 'E'; <- can not replace a character in char*
+    // cout << "char_test: " << char_test << endl; 
+    int size = str_test.size();
+    char *newChar = new char[size + 1];
+    int position = 0;
+    while( position < size+1){
+        if (position == 1){
+            newChar[position] = 'E';
+            position++;
+            continue;
+        }
+        newChar[position] = *char_test;
+        char_test++;
+        position++;
+    }
+    newChar[position] = '\0';
+    cout << newChar << endl;
+}
